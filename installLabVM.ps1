27741fef33ff40e1f84fe50a09c00f2bbd10ae6b
Oliver Lohmann <oliver@lohmann.io>
2019-03-27 14:15:23 +0100

    Chromium to Chrome

--- installLabVM.ps1
+++ installLabVM.ps1
@@ -4,10 +4,12 @@ scoop install sudo
 scoop install git --global
 scoop bucket add extras
 scoop install vscode --global
-scoop install chromium --global
 scoop install curl --global
 scoop install firefox --global
 scoop install helm --global
 scoop install kubectl --global
 scoop install nodejs-lts --global
 scoop install openssh --global
+
+$Path = $env:TEMP; $Installer = "chrome_installer.exe"; Invoke-WebRequest "http://dl.google.com/chrome/install/latest/chrome_installer.exe" -OutFile $Path\$Installer; Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait; Remove-Item $Path\$Installer
+
