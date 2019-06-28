6213ae5f783a59761ee275332dc61ef96a32e6e0
Oliver Lohmann <oliver@lohmann.io>
2019-06-28 08:44:04 +0200

    Install az cli

--- installLabVM.ps1
+++ installLabVM.ps1
@@ -1,10 +1,11 @@
 Invoke-Expression (new-object net.webclient).downloadstring('https://get.scoop.sh')
 
 scoop install sudo
-scoop install git --global
 scoop bucket add extras
+scoop install git --global
 scoop install vscode --global
 scoop install curl --global
+scoop install azure-cli --global
 scoop install firefox --global
 scoop install terraform --global
 scoop install helm --global
@@ -14,3 +15,7 @@ scoop install cmder --global
 scoop install openssh --global
 
 $Path = $env:TEMP; $Installer = "chrome_installer.exe"; Invoke-WebRequest "http://dl.google.com/chrome/install/latest/chrome_installer.exe" -OutFile $Path\$Installer; Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait; Remove-Item $Path\$Installer
+
+# Install VS Code Plugins
+code --install-extension mauve.terraform
+
